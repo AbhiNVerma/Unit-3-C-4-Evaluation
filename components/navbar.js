6545278062cdfd8622a084f94c0ddf62fe049d82
AbhiNVerma <abhisheknandanverma63@gmail.com>
2022-05-02 12:33:34 +0530

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+function navbar(){
+    return `
+    <h2 onclick="location.href='/';">News App</h2>
+    <input type="text" id="search_input" placeholder="Search News">`;
+    
+}
+
+export default navbar;
